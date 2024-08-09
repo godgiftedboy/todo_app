90378e50c3d2079b20c6266efd94b64c47531fad
rajprajapati12321 <rajprajapati@redwire.com.np>
2024-08-09 11:15:15 +0545

    Loaded the Todos from sharedprefs

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -2,34 +2,64 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:lottie/lottie.dart';
 import 'package:todo_ap/main.dart';
+import 'package:todo_ap/models/todo.dart';
 import 'package:todo_ap/pages/add_todo.dart';
+import 'package:todo_ap/services/db_service.dart';
 
-class Homepage extends ConsumerWidget {
+class Homepage extends ConsumerStatefulWidget {
   const Homepage({super.key});
+  @override
+  ConsumerState<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends ConsumerState<Homepage> {
+  var todoListString = "";
+  // var todos = [];
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final todos = ref.watch(todoProvider);
+  void initState() {
+    super.initState();
+    final dbProvider = ref.read(databaseProvider);
+    _loadTodoList(dbProvider);
+  }
+
+  void _loadTodoList(DatabaseService dbProvider) async {
+    dbProvider.getList("todoList").then((value) {
+      todoListString = value ?? "";
+      List<TodoModel> todos = todoListString != ""
+          ? TodoModel.todoListFromJson(todoListString)
+          : [];
+
+      // setState(() {
+      ref.watch(todoProvider.notifier).loadTodo(todos);
+      // });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final todoList = ref.watch(todoProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("My Todos"),
       ),
-      body: todos.isEmpty
+      body: todoList.isEmpty
           ? Center(
               child: LottieBuilder.asset("assets/not_found.json"),
             )
           : ListView.builder(
-              itemCount: todos.length,
+              itemCount: todoList.length,
               itemBuilder: (context, index) {
                 return ListTile(
                   leading: CircleAvatar(child: Text((index + 1).toString())),
-                  title: Text(todos[index].title),
-                  subtitle: Text(todos[index].desc),
+                  title: Text(todoList[index].title),
+                  subtitle: Text(todoList[index].desc),
                   trailing: IconButton(
                       onPressed: () {
                         ref
                             .watch(todoProvider.notifier)
-                            .removeTodo(todos[index]);
+                            .removeTodo(todoList[index]);
                       },
                       icon: const Icon(Icons.remove)),
                 );
