c9e3023a6bc72345e1058d77b86e6f675fcf46a7
rajprajapati12321 <rajprajapati@redwire.com.np>
2024-08-09 11:13:41 +0545

    Updated the TodoNotifier functions to save changes to SharedPrefs

--- lib/providers/todo_provider.dart
+++ lib/providers/todo_provider.dart
@@ -1,16 +1,24 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:todo_ap/models/todo.dart';
+import 'package:todo_ap/services/db_service.dart';
 
 class TodoNotifier extends StateNotifier<List<TodoModel>> {
-  TodoNotifier() : super([]);
+  final DatabaseService dbProvider;
+  TodoNotifier(this.dbProvider) : super([]);
+
+  void loadTodo(List<TodoModel> todos) {
+    state = todos;
+  }
 
   void addTodo(TodoModel todo) {
     if (!state.contains(todo)) {
       state = [...state, todo];
+      dbProvider.saveList("todoList", TodoModel.todoListToJson(state));
     }
   }
 
   void removeTodo(TodoModel todo) {
     state = state.where((element) => element != todo).toList();
+    dbProvider.saveList("todoList", TodoModel.todoListToJson(state));
   }
 }
